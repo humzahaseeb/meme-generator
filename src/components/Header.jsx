4768b5589d3fbf695b557931f5aecbad9afd9e1e
hamza haseeb <hamzahaseeb5@gmail.com>
2022-01-18 18:18:59 +0500

    modified file structure and made header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="http://assets.stickpng.com/images/580b585b2edbce24c47b2a2a.png"
+                className="header--image"
+                alt='troll face'
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h2 className="header--project">Just for fun LOL!</h2>
+        </header>
+    )
+}
+
+export default Header
