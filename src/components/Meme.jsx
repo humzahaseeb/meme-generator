b8cc3676e56f346ae777dbf356b6a7430e2c8471
hamza haseeb <hamzahaseeb5@gmail.com>
2022-01-19 09:02:45 +0500

    added the state of image when the button is clicked

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import memesData from '../memesData'
 
 export const Meme = () => {
+
+    const [memeImage, setMemeImage] = useState(0);
+
+    const getMemeImage = () => {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const randomMeme = memesArray[randomNumber];
+
+        setMemeImage(randomMeme.url);
+    }
+
+
     return (
-        <div className='form'>
-            <div className='input'>
+        <div>
+            <div className='form'>
+                <div className='input'>
                 <input type="text" placeholder='Upper Text' />
                 <input type="text" placeholder='Bottom Text'/>
-            </div>
-            <div className='button'>
+                </div>
+                <div className='button' onClick={getMemeImage}>
                 <input type="button" value="Generate a new meme image" />
+                </div>
             </div>
+
+            <div className='meme-image'>
+                <img src={memeImage} className='meme' />  {/* <-- this is the image that will be generated */}
+            </div>
+
         </div>
     )
 }
